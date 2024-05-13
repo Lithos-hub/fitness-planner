1dd890fdf540353a98fee7697ff9034327d6e78c
Carlos Segura Garcia <lithos.contact@gmail.com>
2024-05-13 15:37:39 +0200

    add import sorting eslint plugin

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -17,14 +17,26 @@ module.exports = {
 		'eslint-config-prettier',
 		'plugin:prettier/recommended',
 	],
-	plugins: ['@typescript-eslint', 'prettier'],
+	plugins: ['@typescript-eslint', 'prettier', 'simple-import-sort'],
 	rules: {
+		'simple-import-sort/imports': 'error',
+		'simple-import-sort/exports': 'error',
 		'vue/html-closing-bracket-newline': [2, { multiline: 'never' }],
 		'tailwindcss/no-custom-classname': 'off',
 		'prettier/prettier': [
 			'error',
 			{
 				endOfLine: 'auto',
+			},
+		],
+		'sort-imports': [
+			'error',
+			{
+				ignoreCase: true,
+				ignoreDeclarationSort: true,
+				ignoreMemberSort: false,
+				memberSyntaxSortOrder: ['none', 'all', 'multiple', 'single'],
+				allowSeparatedGroups: false,
 			},
 		],
 	},
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,11 @@
-import { createApp } from 'vue';
 import './style.css';
-import App from './App.vue';
 
 import PrimeVue from 'primevue/config';
+import { createApp } from 'vue';
+
 import Lara from '@/presets/lara';
+
+import App from './App.vue';
 
 createApp(App)
 	.use(PrimeVue, {
